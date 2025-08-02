efb40f93271cdd32e0f3b524d687e3705b0691a8
Srikanth <khandeshSrikanth9@gmail.com>
2025-08-02 20:45:07 +0530

    two-sum maxSubArry RemoveDuplicates

---
+++ Readme.md
@@ -0,0 +1,17 @@
+15 Days Coding Challenge - DSA Practice
+
+Welcome to my 15-day coding challenge focused on Data Structures and Algorithms (DSA) practice! The goal is to solve 3 LeetCode problems daily for 15 days, improving my problem-solving skills and tracking progress.
+
+Progress
+
+Day 1:
+
+Two Sum - Solved
+
+Remove Duplicates from Sorted Array - Solved
+
+Maximum Subarray - Solved
+
+Future Plans
+
+Continue solving 3 problems daily.
---
+++ Two-Sum.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+ class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int ans[]=new int[2];
+        for(int i=0;i<nums.length;i++){
+            int currEle=nums[i];
+           int remainingSum=target-nums[i];
+            if(map.containsKey(remainingSum)){
+                ans[0]=map.get(remainingSum);
+                ans[1]=i;
+                return ans;
+            }
+            map.put(nums[i],i);
+        }
+        return ans;
+
+    }
+}
